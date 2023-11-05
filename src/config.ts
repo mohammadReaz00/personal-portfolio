17b5d0c6d1aa9a46f6d9f5b954e6c59814f2c61e
MohammadReaz <mdreaz549@gmail.com>
2023-11-05 23:54:14 +0600

    modified config file

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = "Mohammad Reaz Personal Website";
+export const SITE_DESCRIPTION =
+  "Mohammad Reaz Is an Aspiring data professional with a strong passion for unlocking the insights hidden within data. I have always been intrigued by the power of data to drive informed decision-making and fuel innovation.";
